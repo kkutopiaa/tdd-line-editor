416af54ee2d6345276439018e8a22b9c047bcc1d
qinxuekuan <qinxuekuan@qianxin.com>
2022-11-16 13:20:38 +0800

    refactor:重构：抽取重复代码。添加 get 方法。 存在就直接查找，不存在就先创建。

--- src/line-editor.ts
+++ src/line-editor.ts
@@ -9,9 +9,9 @@ export class LineEditor extends Konva.Group {
         let previous = -1;
 
         for (let i = 0; i < points.length / 2; i++) {
-            this.create(`${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
+            this.get(`${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
             if (previous !== -1) {
-                this.create(`${i}-control`)
+                this.get(`${i}-control`)
                     .setAttrs({
                         x: points[previous * 2] + (points[i * 2] - points[previous * 2]) / 2,
                         y: points[previous * 2 + 1] + (points[i * 2 + 1] - points[previous * 2 + 1]) / 2,
@@ -31,9 +31,9 @@ export class LineEditor extends Konva.Group {
         let points = this.line!.points();
         let previous = -1;
         for (let i = 0; i < points.length / 2; i++) {
-            this.findOne(`.${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
+            this.get(`${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
             if (previous !== -1) {
-                this.findOne(`.${i}-control`).setAttrs({
+                this.get(`${i}-control`).setAttrs({
                     x: points[previous * 2] + (points[i * 2] - points[previous * 2]) / 2,
                     y: points[previous * 2 + 1] + (points[i * 2 + 1] - points[previous * 2 + 1]) / 2,
                 });
@@ -42,6 +42,9 @@ export class LineEditor extends Konva.Group {
         }
     }
 
+    private get(name: string) {
+        return this.findOne(`.${name}`) || this.create(name);
+    }
 
     private create(name: string) {
         let point = new Konva.Circle({name, radius: 10});
