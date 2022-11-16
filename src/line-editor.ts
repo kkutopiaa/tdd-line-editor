b37f455d03fd25c202587de01001594bef9aa393
qinxuekuan <qinxuekuan@qianxin.com>
2022-11-16 13:35:39 +0800

    feat:实现 should change line points when dragging anchor 测试

--- src/line-editor.ts
+++ src/line-editor.ts
@@ -35,7 +35,13 @@ export class LineEditor extends Konva.Group {
     private create(index: number, type: string) {
         let point = new Konva.Circle({name: `${index}-${type}`, radius: 10});
         if (type === 'anchor') {
-            point.draggable(true);
+            point.draggable(true)
+                .on('dragmove', (e) => {
+                    let points = this.line!.points();
+                    points[index * 2] = e.target.x();
+                    points[index * 2 + 1] = e.target.y();
+                    this.line!.points(points);
+                });
         }
         this.add(point);
         return point;
