505864f7d2326cbc16a659b08d8be0253a6f88a7
qinxuekuan <qinxuekuan@qianxin.com>
2022-11-16 13:11:05 +0800

    feat:实现 should update control position when line update 测试

--- src/line-editor.ts
+++ src/line-editor.ts
@@ -30,8 +30,16 @@ export class LineEditor extends Konva.Group {
 
     update() {
         let points = this.line!.points();
+        let previous = -1;
         for (let i = 0; i < points.length / 2; i++) {
-            this.findOne(`.${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]})
+            this.findOne(`.${i}-anchor`).setAttrs({x: points[i * 2], y: points[i * 2 + 1]});
+            if (previous !== -1) {
+                this.findOne(`.${i}-control`).setAttrs({
+                    x: points[previous * 2] + (points[i * 2] - points[previous * 2]) / 2,
+                    y: points[previous * 2 + 1] + (points[i * 2 + 1] - points[previous * 2 + 1]) / 2,
+                });
+            }
+            previous = i;
         }
 
     }
