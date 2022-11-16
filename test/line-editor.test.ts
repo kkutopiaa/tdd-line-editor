efb10a3bb3f99033f0b69f8dcc8a19c0d979a876
qinxuekuan <qinxuekuan@qianxin.com>
2022-11-16 12:42:47 +0800

    feat:实现 should add anchor to line editor 测试

--- test/line-editor.test.ts
+++ test/line-editor.test.ts
@@ -4,14 +4,13 @@ import {LineEditor} from "../src/line-editor";
 
 describe('Line editor', () => {
 
-    // TODO set anchor on line editor
     it('should add anchor to line editor', function () {
         let line = new Konva.Line({points: [10, 10, 30, 30]});
         let editor = new LineEditor();
         editor.attach(line);
 
-        expect(editor.findOne('.0-anchor')).toMatchObject({x: 10, y: 10});
-        expect(editor.findOne('.1-anchor')).toMatchObject({x: 30, y: 30});
+        expect(editor.findOne('.0-anchor').getAttrs()).toMatchObject({x: 10, y: 10});
+        expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 30, y: 30});
     });
 
     // TODO update anchor when line update
