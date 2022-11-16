c8f69333fd0278375c3c45bd7a9e44b4ff9cf2ce
qinxuekuan <qinxuekuan@qianxin.com>
2022-11-16 14:02:07 +0800

    feat:完善 should remove anchor when double click 测试：删除锚点时，之前的控制点信息也跟着变化。（实现逻辑之前已有）

--- test/line-editor.test.ts
+++ test/line-editor.test.ts
@@ -83,6 +83,7 @@ describe('Line editor', () => {
         expect(line.points()).toEqual([10, 10, 30, 30]);
         expect(editor.findOne('.1-anchor').getAttrs()).toMatchObject({x: 30, y: 30});
         expect(editor.findOne('.2-anchor')).toBeUndefined();
+        expect(editor.findOne('.2-control')).toBeUndefined();
 
     });
 
